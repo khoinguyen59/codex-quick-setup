9dc2e9f39f8138361bedefec18938951f2323577
khoinguyen59 <23520783@gm.uit.edu.vn>
2026-04-28 15:27:34 +0700

    Fix Windows bat file elevation and crashing

--- install-windows.bat
+++ install-windows.bat
@@ -1,21 +1,16 @@
 @echo off
 :: Batch file to run PowerShell script as Administrator
-:: Elevate privileges
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-if '%errorlevel%' NEQ '0' (
-    echo Đang yêu cầu quyền Administrator...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "powershell.exe", "-ExecutionPolicy Bypass -NoProfile -File ""%~dp0install-windows.ps1""", "", "runas", 1 >> "%temp%\getadmin.vbs"
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    goto :run
+) else (
+    echo Dang yeu cau quyen Administrator...
+    powershell -Command "Start-Process powershell -ArgumentList '-NoExit -ExecutionPolicy Bypass -NoProfile -File ""%~dp0install-windows.ps1""' -Verb RunAs"
     exit /B
+)
 
-:gotAdmin
-    pushd "%CD%"
-    CD /D "%~dp0"
-    powershell.exe -ExecutionPolicy Bypass -NoProfile -File "install-windows.ps1"
-    pause
+:run
+pushd "%CD%"
+CD /D "%~dp0"
+powershell.exe -ExecutionPolicy Bypass -NoProfile -File "install-windows.ps1"
+pause
